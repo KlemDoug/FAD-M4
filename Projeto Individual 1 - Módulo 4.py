adc0baa504bd320cf7adcbce1c05b8c1647e1ef4
Douglas Klem <120439075+KlemDoug@users.noreply.github.com>
2023-04-27 04:47:04 -0300

    Update Projeto Individual 1 - Módulo 4.py

--- Projeto Individual 1 - Módulo 4.py
+++ Projeto Individual 1 - Módulo 4.py
@@ -27,10 +27,11 @@ para mostrar ao dono da loja como a análise dos dados pode ser útil para eles.
 |Sábado     |100      |245.00  |525         |0	    |
 |Domingo    |0        |164.00  |75	        |820    |
 
-Além disso, ele informou que os ganhos não estão nessa planilha, mas que ele possui a seguinte lista: **2200, 2420.50, 3391, 5322, 4898.50, 4200, 3893**
-respectivos aos dias da semana. Ele te deixou bem livre para incluir no relatório as estatísticas que desejar, mas ele deseja que o relatório contenha outros dados que veremos a seguir:
+Além disso, ele informou que os ganhos não estão nessa planilha, mas que ele possui a seguinte lista: 2200, 2420.50, 3391, 5322, 4898.50, 4200, 3893
+respectivos aos dias da semana. Ele te deixou bem livre para incluir no relatório as estatísticas que desejar, mas ele deseja que o relatório contenha
+outros dados que veremos a seguir:
 
-# Dados para o Relatório
+## Dados para o Relatório
 
 Gerar um relatório de uma semana para mostrar ao dono da loja como a análise dos dados pode ser útil e incluir outros itens:
 
@@ -42,7 +43,7 @@ Gerar um relatório de uma semana para mostrar ao dono da loja como a análise d
 * O lucro diário para informar qual dia foi mais lucrativo e o lucro total da semana;
 * Uma organização com textos explicando o que foi feito para obter os valores e os resultados bem apresentados.
 
-# Plano de Ação
+## Plano de Ação
 
 1. Criação de um *dataframe* para a tabela de despesas fornecida pelo gerente;
 2. Adição da coluna com os valores de ganhos fornecidos;
